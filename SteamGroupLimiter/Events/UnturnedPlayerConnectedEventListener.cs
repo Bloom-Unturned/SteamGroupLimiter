0b199fd163c52cfce90b9245908542ca7dd147a6
Hath <Hath@Hath.com>
2024-05-20 15:57:46 -0300

    Agregar archivos de proyecto.

---
+++ SteamGroupLimiter/Events/UnturnedPlayerConnectedEventListener.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Cysharp.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using OpenMod.API.Commands;
+using OpenMod.API.Eventing;
+using OpenMod.API.Plugins;
+using OpenMod.Unturned.Players;
+using OpenMod.Unturned.Players.Connections.Events;
+using OpenMod.Unturned.Plugins;
+using OpenMod.Unturned.Users;
+using SDG.Unturned;
+using Steamworks;
+
+// For more, visit https://openmod.github.io/openmod-docs/devdoc/guides/getting-started.html
+namespace SteamGroupLimiter.Events
+{
+    public class UnturnedPlayerConnectedEventListener : IEventListener<UnturnedPlayerConnectedEvent> 
+    {
+        public UnturnedPlayerConnectedEventListener()
+        {
+        }
+
+        public Task HandleEventAsync(object sender, UnturnedPlayerConnectedEvent @event)
+        {
+            UnturnedPlayer player = @event.Player;
+
+            if (Provider.modeConfigData.Gameplay.Max_Group_Members != 0 && GetMemberCount(player.Player.quests.groupID) > Provider.modeConfigData.Gameplay.Max_Group_Members)
+            {
+                player.Player.quests.leaveGroup(true);
+                player.PrintMessageAsync("Tu grupo excede el limite de 8 miembros!");
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public byte GetMemberCount(CSteamID ID)
+        {
+            if (ID == CSteamID.Nil) return 0;
+            return (byte)Provider.clients.Where(x => x.player.quests.isMemberOfGroup(ID)).Count();
+        }
+    }
+
+}
---
+++ SteamGroupLimiter/SteamGroupLimiter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Cysharp.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using OpenMod.API.Plugins;
+using OpenMod.Unturned.Plugins;
+
+// For more, visit https://openmod.github.io/openmod-docs/devdoc/guides/getting-started.html
+
+[assembly: PluginMetadata("SteamGroupLimiter", DisplayName = "SteamGroupLimiter")]
+
+namespace SteamGroupLimiter
+{
+    public class SteamGroupLimiter : OpenModUnturnedPlugin
+    {
+        private readonly IConfiguration m_Configuration;
+        private readonly IStringLocalizer m_StringLocalizer;
+        private readonly ILogger<SteamGroupLimiter> m_Logger;
+
+        public SteamGroupLimiter(
+            IConfiguration configuration,
+            IStringLocalizer stringLocalizer,
+            ILogger<SteamGroupLimiter> logger,
+            IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+            m_Configuration = configuration;
+            m_StringLocalizer = stringLocalizer;
+            m_Logger = logger;
+        }
+
+        protected override async UniTask OnLoadAsync()
+        {
+            // await UniTask.SwitchToMainThread(); uncomment if you have to access Unturned or UnityEngine APIs
+            m_Logger.LogInformation("Hello World!");
+
+            // await UniTask.SwitchToThreadPool(); // you can switch back to a different thread
+        }
+
+        protected override async UniTask OnUnloadAsync()
+        {
+            // await UniTask.SwitchToMainThread(); uncomment if you have to access Unturned or UnityEngine APIs
+            m_Logger.LogInformation(m_StringLocalizer["plugin_events:plugin_stop"]);
+        }
+    }
+}
